7b2d81696396b6ab1ae5ac0a99077e63df9a0715
kimjaeyou <unknown88455144@gmail.com>
2024-08-08 09:01:09 +0900

    git push Project -KJY::08/08/09:00

---
+++ KostaProject/src/main/java/ProTest/KostaProjectApplication.java
@@ -0,0 +1,13 @@
+package ProTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KostaProjectApplication.class, args);
+	}
+
+}
